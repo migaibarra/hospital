3b52e28983190bf31b887b3418dce902a51a4d53
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-06 16:25:35 -0700

    tests association with patients

---
+++ hospital-interface-js/spec/patientSpec.js
@@ -0,0 +1,11 @@
+describe('Patient', () => {
+  let sick_patient;
+
+  beforeEach(() => {
+    sick_patient = new Patient ("Andrew", "Sam", "Anderson");
+  });
+
+  describe("attributes", () => {
+
+  });
+});
