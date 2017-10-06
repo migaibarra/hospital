45494b12c5265336554e4a60f4d19b42e58f9be8
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-06 15:40:39 -0700

    tests that the associations with doctors and patients are null

--- hospital-interface-js/app/hospital.js
+++ hospital-interface-js/app/hospital.js
@@ -1,8 +1,14 @@
 class Hospital {
   constructor(name) {
     this.name = name;
-  }
-}
+    this.doctors = [];
+    this.patients = [];
+  };
+
+  hireDoctor(doctor) {
+    this.push(doctor);
+  };
+};
 
 // This pattern of adding a class to Window is **NOT** standard and not best-practice
 // The standard way of doing so is ES2015 modules, using the export keyword
--- hospital-interface-js/spec/hospitalSpec.js
+++ hospital-interface-js/spec/hospitalSpec.js
@@ -7,7 +7,19 @@ describe('Hospital', () => {
     hospital_test = new Hospital("St. Johns Hospital");
   });
 
-  it('has a name', () => {
-    expect(hospital_test.name).toBe("St. Johns Hospital");
+  describe('attributes', () => {
+    it('has a name', () => {
+      expect(hospital_test.name).toBe("St. Johns Hospital");
+    });
+  });
+
+  describe('associations', () => {
+    it('is instantiated with no doctors (null)', () => {
+      expect(hospital_test.doctors.length).toBe(0);
+    });
+
+    it('is instantiated with no patients (null)', () => {
+      expect(hospital_test.patients.length).toBe(0);
+    });
   });
 });
